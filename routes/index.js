b15966237807bf43e89f19ee75ad0af98b42206f
Darshil Gadhiya <gadhiyadarshil@gmail.com>
2021-11-22 23:09:43 -0500

    This is My Project

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/',function(req,res,next){
+  res.render('index',{title: 'Home.'})
+});
+
+router.get('/index',function(req,res,next){
+  res.render('index',{title: 'Home.'})
+});
+
+router.get('/about',function(req,res,next){
+  res.render('about',{title: 'About Me.'})
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact me'});
+});
+
+router.get('/project', function(req, res, next) {
+  res.render('project',{ title: 'Projects'});
+
+});
+module.exports = router;
